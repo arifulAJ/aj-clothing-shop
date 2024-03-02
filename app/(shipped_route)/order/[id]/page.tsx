1ca7651c9ea08c7c006ad2e77b43697e353d077a
Ariful islam <arifulislam10111999@gmail.com>
2024-03-02 09:34:32 +0600

    add new code

--- app/(shipped_route)/order/[id]/page.tsx
+++ app/(shipped_route)/order/[id]/page.tsx
@@ -1,33 +1,5 @@
-// import OrderDetails from "./OrderDetails";
-
-// export function generateMeatadata({ params }: { params: { id: string } }) {
-//   return {
-//     title: `Order${params.id}`,
-//   };
-// }
-// export default function OrderDetailsPage({
-//   params,
-// }: {
-//   params: { id: string };
-// }) {
-//   return (
-//     <OrderDetails
-//       paypalClientId={process.env.PAYPAL_CLIENT_ID || "sb"}
-//       orderId={params.id}
-//     />
-//   );
-// }
-// Import OrderDetails component
 import OrderDetails from "./OrderDetails";
 
-// // Define the generateMetadata function
-// export function generateMetadata({ params }: { params: { id: string } }) {
-//   return {
-//     title: `Order ${params.id}`,
-//   };
-// }
-
-// Define the OrderDetailsPage component
 export default function OrderDetailsPage({
   params,
 }: {
